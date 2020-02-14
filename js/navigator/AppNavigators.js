c18a8d4046b88770907c1b98590317ca32d0ed54
candy4390 <2459402883@qq.com>
2020-02-14 14:24:23 +0800

    :sparkles: 底部tabbar+二级页面导航结构+react navigation主题

--- js/navigator/AppNavigators.js
+++ js/navigator/AppNavigators.js
@@ -8,6 +8,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {C1Page} from '../pages/my/C1Page';
 import {NavigationContainer} from '@react-navigation/native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {MyTheme} from '../theme/theme';
 
 export const rootCom = 'Init'; //设置根路由，对应RootNavigator中第一个初始化的路由名
 
@@ -59,62 +60,52 @@ function MyStackScreen() {
         component={MyPage}
         options={{headerShown: false}}
       />
-      <MyStack.Screen
-        name="C1"
-        component={C1Page}
-        options={{headerShown: false}}
-      />
     </MyStack.Navigator>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
+function HomeTabs() {
   return (
     <Tab.Navigator
-      initialRouteName={rootCom}
-      tabBarOptions={{
-        inactiveTintColor: '#000',
-        activeTintColor: '#BB9246',
-        showIcon: true,
-      }}>
-      <Tab.Screen
-        name="首页"
-        component={HomeStackScreen}
-        options={{
-          tabBarIcon: () => <AntDesign name="home" size={25} />,
-        }}
-      />
-      <Tab.Screen
-        name="应用"
-        component={ApplicationStackScreen}
-        options={{
-          tabBarIcon: () => <AntDesign name="appstore-o" size={25} />,
-        }}
-      />
-      <Tab.Screen
-        name="发现"
-        component={DiscoverStackScreen}
-        options={{
-          tabBarIcon: () => <AntDesign name="eyeo" size={25} />,
-        }}
-      />
-      <Tab.Screen
-        name="我的"
-        component={MyStackScreen}
-        options={{
-          tabBarIcon: () => <AntDesign name="user" size={25} />,
-        }}
-      />
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+          switch (route.name) {
+            case '首页':
+              iconName = 'home';
+              break;
+            case '应用':
+              iconName = 'appstore-o';
+              break;
+            case '发现':
+              iconName = 'eyeo';
+              break;
+            case '我的':
+              iconName = 'user';
+              break;
+          }
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+      })}>
+      <Tab.Screen name="首页" component={HomeStackScreen} />
+      <Tab.Screen name="应用" component={ApplicationStackScreen} />
+      <Tab.Screen name="发现" component={DiscoverStackScreen} />
+      <Tab.Screen name="我的" component={MyStackScreen} />
     </Tab.Navigator>
   );
 }
 
+const RootStack = createStackNavigator();
+
 export default function AppNavigators() {
   return (
-    <NavigationContainer>
-      <MyTabs />
+    <NavigationContainer theme={MyTheme}>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeTabs} />
+        <RootStack.Screen name="C1" component={C1Page} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
--- js/theme/theme.js
+++ js/theme/theme.js
@@ -1,9 +1,10 @@
-import {DefaultTheme} from '@react-navigation/native';
-
 export const MyTheme = {
-  ...DefaultTheme,
+  dark: false,
   colors: {
-    ...DefaultTheme.colors,
-    primary: 'rgb(255, 45, 85)',
+    primary: '#BB9246',
+    background: 'rgb(242, 242, 242)',
+    card: 'rgb(255, 255, 255)',
+    text: 'rgb(28, 28, 30)',
+    border: 'rgb(199, 199, 204)',
   },
 };
